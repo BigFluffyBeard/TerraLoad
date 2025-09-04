c5b2b1c59f832aad7e3d7f45e2fe91c41903f44d
BigFluffyBeard <shyane.cornell@outlook.com>
2025-09-03 19:12:02 -0700

    Delete Scripts/Download_ephemeris.py

--- Scripts/Download_ephemeris.py
+++
@@ -1,6 +0,0 @@
-import os
-import requests
-from pathlib import Path
-from datetime import datetime
-
-### This is entirely to download satellite orbital tracks to plot for visual purposes. If I do post this on github, I'll probably edit this whole thing so the user needs to input the stuff they want. I aint leavin my shit in here.
